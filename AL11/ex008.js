43e2253d9daa85937293b5fbd8e5fd97d260a65b
Allan Ferreira <al.portoferreira@gmail.com>
2025-01-23 20:26:45 -0300

    Codando em JS

---
+++ AL11/ex008.js
@@ -0,0 +1,6 @@
+const vel = 78.2
+console.log(`A velocidade do seu carro é ${vel}Km/h `)
+if (vel > 60){       //condição simples
+      console.log(`Voce ultrapassou a velocidade permitida. MULTADO!`)
+}
+console.log(`Dirija sempre usando o cinto de segurança!`)
---
+++ AL11/ex009.js
@@ -0,0 +1,7 @@
+const país = 'EUA'
+console.log(`Vivendo em ${país} `)
+if (país == 'EUA'){
+    console.log('Você é Norte Americano')
+} else {
+    console.log('Você é Estrangeiro')
+}
