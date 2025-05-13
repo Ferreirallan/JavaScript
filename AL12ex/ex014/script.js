3c7d4cf6ad6f2cf36386aa4a607a10146829ae0b
Allan Ferreira <al.portoferreira@gmail.com>
2025-05-13 10:01:16 -0300

    criando o codigo javascript

---
+++ AL12ex/ex014/script.js
@@ -0,0 +1,16 @@
+function carregar() {
+var msg = window.document.getElementById("msg");
+const img = window.document.getElementById("imagem");
+const data = new Date();
+var hora = data.getHours();
+msg.innerHTML = `Agora são ${hora} horas.`;
+if (hora >= 0 && hora < 12) {
+    // Bom dia!
+    img.src = "imagens/manha.png";
+    document.body.style.background = "#e2cd9f";
+    }else if (hora >= 12 && hora < 18) {
+        // Boa Tarde!
+    }else{
+        // Boa Noite!
+    }
+}
