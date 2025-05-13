1600b7aaea13f31a8c0c6ea32a234cf2080cec40
Allan Ferreira <al.portoferreira@gmail.com>
2025-05-13 14:31:06 -0300

    FINISH

--- AL12ex/ex014/script.js
+++ AL12ex/ex014/script.js
@@ -6,11 +6,15 @@ const hora = data.getHours();
 msg.innerHTML = `Agora são ${hora} horas.`;
 if (hora >= 0 && hora < 12) {
     // Bom dia!
-    img.src = "imagens/manha.png";
-    document.body.style.background = "#e2cd9f";
-    }else if (hora >= 12 && hora < 18) {
+    img.src = "fotomanhã.jpg"
+    document.body.style.background = "#e2cd9f"
+    } else if (hora >= 12 && hora < 18) {
         // Boa Tarde!
-    }else{
+        img.src = "fototarde.jpg"
+        document.body.style.background = "#b9846f"
+    } else {
         // Boa Noite!
+        img.src = "fotonoite.jpg"
+        document.body.style.background = "#515154"
     }
 }
