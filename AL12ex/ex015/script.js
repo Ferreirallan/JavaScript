8282f262b5f7ba65455bdb86dc06ad939de93678
Allan Ferreira <al.portoferreira@gmail.com>
2025-05-14 13:48:22 -0300

    part js

--- AL12ex/ex015/script.js
+++ AL12ex/ex015/script.js
@@ -1,1 +1,15 @@
-alert('Olá, Mundo!');
+function verificar() {
+   const data = new Date();
+   const ano = data.getFullYear();
+   const fano  = document.getElementById('txtano');
+   const res = document.querySelector('div#res');   
+   if (fano.value.length == 0 || Number (fano.value) > ano) {
+      window.alert('[ERRO] Verifique os dados e tente novamente!');
+   }    else{
+            const fsex = document.getElementsByName('radsex');
+            const idade = ano - Number(fano.value);
+            const genero
+   }
+   
+}
+
