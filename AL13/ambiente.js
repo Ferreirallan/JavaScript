cc8ea6b99c8f2cd3be1aace2f4283ba24933a9ec
Allan Ferreira <al.portoferreira@gmail.com>
2025-05-21 15:42:56 -0300

    exercicio 13

---
+++ AL13/ambiente.js
@@ -0,0 +1,21 @@
+let c = 1
+do {
+    console.log(`Passo ${c}`)
+    c++
+}  while(c <= 1)
+
+
+
+
+
+
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
